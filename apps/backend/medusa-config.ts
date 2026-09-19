56be8b5ebf8b76754eb2ddf5897a30b69163de3c
happy-pixel88 <happyhangs45@gmail.com>
2026-09-19 18:30:38 +0500

    Update medusa-config.ts

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -6,6 +6,7 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
+    debug: true,
     http: {
       storeCors: process.env.STORE_CORS || "*",
       adminCors: process.env.ADMIN_CORS || "*",
